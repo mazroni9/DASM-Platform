9f837836ff5990e55282a2686bd78b4a6bc9d304
DASM-e <zahrma0p@yahoo.com>
2025-08-12 12:52:36 +0300

    Update cors.php allow all

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [env('FRONTEND_URL'), 'https://dasm-platform-git-development-mohammed-alzahranis-projects.vercel.app','https://dasm-laravel.onrender.com'],
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
